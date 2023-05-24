8a5c2cf64c54aa2455440dc993e2a681e8f6915a
ClassicUA <ClassicUA@users.noreply.github.com>
2023-05-24 22:42:10 +0300

    Ще переклад.

--- tatiana5/favicon/language/uk/info_acp_favicon.php
+++ tatiana5/favicon/language/uk/info_acp_favicon.php
@@ -24,12 +24,12 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 	'ACP_FAVICON'				=> 'Favicon',
-	'ACP_FAVICON_EXPLAIN'		=> 'Иконка конференции',
+	'ACP_FAVICON_EXPLAIN'		=> 'Іконка конференції',
 
-	'ACP_FAVICON_EXT'			=> 'Выбрать расширение файла иконки конференции',
-	'ACP_FAVICON_EXT_EXPLAIN'	=> '<strong>favicon.ico</strong> или <strong>favicon.png</strong>',
-	'ACP_FAVICON_APPLE'			=> 'Включить иконку для apple',
+	'ACP_FAVICON_EXT'			=> 'Обрати розширення файлу іконки конференції',
+	'ACP_FAVICON_EXT_EXPLAIN'	=> '<strong>favicon.ico</strong> або <strong>favicon.png</strong>',
+	'ACP_FAVICON_APPLE'			=> 'Увімкнути іконку для apple',
 	'ACP_FAVICON_APPLE_EXPLAIN'	=> '<strong>apple_touch_icon.png</strong>',
 
-	'ACP_FAVICON_NOT_FOUND'		=> 'Favicon не найден',
+	'ACP_FAVICON_NOT_FOUND'		=> 'Favicon не знайдено',
 ));
